a9d319fcecac057c94996c6bcd9f1f2604070a2e
Andy Prasetya <andyprasetya@aol.com>
2020-04-18 10:30:22 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './jquery-global.js';
 import 'bootstrap';
 import '../node_modules/bootstrap/scss/bootstrap.scss';
 import '../node_modules/leaflet/dist/leaflet.css';
-import './stylesheet/custom.css';
+import './stylesheet/custom.scss';
 import './stylesheet/font-awesome.min.css';
 // UI entry point
 import commonui from './ui';
